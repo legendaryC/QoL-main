22c042cd67d48dd4e3a48285d93ce3cbb553cd13
legendaryc <763094977@qq.com>
2021-01-17 16:36:09 -0600

    add self

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ class ImportData:
     def __init__(self):
         super().__init__()
 
-    def get_data():
+    def get_data(self):
         new_entry = Albumin(patient_ID='pa0001',
                             date_time=datetime.datetime(2020, 5, 17), value=1.2)
         new_entry.save()
